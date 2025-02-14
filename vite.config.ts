137c24cd3bc7a48e7c98fc0bada9edc91ff5beb8
Jeff Ma <jucjeff96@gmail.com>
2025-02-14 11:46:31 -0600

    fix: Modified deploy setups so local works properly

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/fetch-dogs/",
+  base: process.env.NODE_ENV === "production" ? "/fetch-dogs/" : "/",
   plugins: [react()],
-})
+});
