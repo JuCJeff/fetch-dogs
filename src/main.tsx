c6e540e87bba22633cba8d39d300b7babe848b4c
Jeff Ma <jucjeff96@gmail.com>
2025-02-14 14:54:11 -0600

    chore: Modify local dev setup

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router>
+    <Router basename="/fetch-dogs/">
       <GlobalProvider>
         <App />
       </GlobalProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? "/fetch-dogs/" : "/",
   plugins: [react()],
+  server: {
+    open: "/fetch-dogs/",
+  },
 });
